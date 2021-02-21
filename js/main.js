a6d5912167134eaf56bb70c231586424596bd88a
ゆーじろー <67575620+yujiroN@users.noreply.github.com>
2021-02-21 16:21:34 +0900

    first
    first

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
